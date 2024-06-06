9a3a38e5ed5968476f627b213879e6d28ba6ef3a
CorreaJose13 <correa.jose@correounivalle.edu.co>
2024-06-05 22:12:31 -0500

    Add class to receive and validate user input through keyboard

---
+++ src/main/java/com/aluracursos/literalura/util/Input.java
@@ -0,0 +1,21 @@
+package com.aluracursos.literalura.util;
+
+import java.util.Scanner;
+
+public class Input {
+    public static int enterValidInput (Scanner reader){
+        int option=0;
+        boolean validInput = false;
+        while (!validInput) {
+            System.out.println("Please enter an option:");
+            if (reader.hasNextInt()) {
+                option = reader.nextInt();
+                validInput = true;
+            } else {
+                System.out.println("That's not a valid integer. Please try again.");
+                reader.next();
+            }
+        }
+        return option;
+    }
+}
