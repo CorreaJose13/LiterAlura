c7da82e9e76e12acf2fa8cd3527083a9f81f62b9
CorreaJose13 <correa.jose@correounivalle.edu.co>
2024-06-05 22:16:50 -0500

    Add controller class and searchBook method

---
+++ src/main/java/com/aluracursos/literalura/controller/Controller.java
@@ -0,0 +1,26 @@
+package com.aluracursos.literalura.controller;
+
+import com.aluracursos.literalura.model.BodyDTO;
+import com.aluracursos.literalura.model.BookDTO;
+import com.aluracursos.literalura.service.ApiConsumer;
+import com.aluracursos.literalura.service.OMapper;
+
+import java.net.http.HttpResponse;
+
+public class Controller {
+    private final String baseURL = "https://gutendex.com/books/";
+    private ApiConsumer consumer= new ApiConsumer();
+    private OMapper mapper = new OMapper();
+
+    public BookDTO searchBook (String bookName) {
+        String bookFormatted = bookName.replace(" ","%20");
+        HttpResponse<String> response = consumer.getResponse(baseURL+"?"+bookFormatted);
+        String body = consumer.responseBody(response);
+        var bodyDTO = mapper.convertData(body, BodyDTO.class);
+        if (bodyDTO.count() == 0){
+            return null;
+        }
+        var bookList = bodyDTO.bookList();
+        return bookList.get(0);
+    }
+}
