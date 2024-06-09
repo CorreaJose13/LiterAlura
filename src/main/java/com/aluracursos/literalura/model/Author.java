5e269e7fb5fb0f20c77bde60a50687368e3f30df
CorreaJose13 <correa.jose@correounivalle.edu.co>
2024-06-08 19:00:07 -0500

    Add author class

--- src/main/java/com/aluracursos/literalura/model/Author.java
+++ src/main/java/com/aluracursos/literalura/model/Author.java
@@ -1,4 +1,39 @@
 package com.aluracursos.literalura.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name="authors")
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String name;
+    private int birth_year;
+    private int death_year;
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Book> bookList;
+
+    public Author(){}
+
+    public Author(AuthorDTO author){
+        this.name= author.name();
+        this.birth_year= author.birthYear();
+        this.death_year= author.deathYear();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBirth_year() {
+        return birth_year;
+    }
+
+    public int getDeath_year() {
+        return death_year;
+    }
 }
