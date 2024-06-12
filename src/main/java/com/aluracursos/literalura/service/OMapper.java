7224ca761f18039767ddc2c5420c4a40d6ad5649
CorreaJose13 <correa.jose@correounivalle.edu.co>
2024-06-11 22:06:27 -0500

    Turn field mapper to final

--- src/main/java/com/aluracursos/literalura/service/OMapper.java
+++ src/main/java/com/aluracursos/literalura/service/OMapper.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class OMapper implements IMapper{
-    private ObjectMapper mapper = new ObjectMapper();
+    private final ObjectMapper mapper = new ObjectMapper();
     @Override
     public <T> T convertData(String json, Class<T> customClass) {
         try {
