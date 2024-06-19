e6697918f0e6c7f121f0d8aaaf9fbe817d7c1d9c
CorreaJose13 <correa.jose@correounivalle.edu.co>
2024-06-19 10:43:08 -0500

    Add JpaRepository

--- src/main/java/com/aluracursos/literalura/repository/Repository.java
+++ src/main/java/com/aluracursos/literalura/repository/Repository.java
@@ -1,7 +1,14 @@
 package com.aluracursos.literalura.repository;
 
 import com.aluracursos.literalura.model.Author;
+import com.aluracursos.literalura.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface Repository extends JpaRepository<Author, Long> {
+
+    @Query("SELECT l FROM Author a JOIN a.bookList l")
+    List<Book> buscarTodosLosLibros();
 }
