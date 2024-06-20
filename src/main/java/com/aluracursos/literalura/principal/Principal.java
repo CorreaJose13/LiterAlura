2d73617e9dd3eeaed0ef33b1ee600db09f2588a8
CorreaJose13 <correa.jose@correounivalle.edu.co>
2024-06-19 21:41:23 -0500

    Major changes to Principal.java class
    Working project from option 1 to 3 from the main menu, added two functions that return lists of books and authors stored in the database and finally the 'searchBookByTitle' function that ask to the user if he wanna save the searched book or not, and validates if either the book or the author already exists in the DB and do some operations for each case.

--- src/main/java/com/aluracursos/literalura/principal/Principal.java
+++ src/main/java/com/aluracursos/literalura/principal/Principal.java
@@ -3,12 +3,18 @@ package com.aluracursos.literalura.principal;
 import com.aluracursos.literalura.controller.Controller;
 import com.aluracursos.literalura.model.Author;
 import com.aluracursos.literalura.model.Book;
+import com.aluracursos.literalura.model.BookDTO;
 import com.aluracursos.literalura.repository.Repository;
 import com.aluracursos.literalura.util.Input;
 import com.aluracursos.literalura.util.UserMessages;
 
+import javax.naming.NameNotFoundException;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private final Scanner reader = new Scanner(System.in);
@@ -46,7 +52,7 @@ public class Principal {
                     listBooksByLang();
                     break;
                 default:
-                    System.out.println("Invalid option. Please enter a valid value.");
+                    System.out.println(UserMessages.invalidOption());
                     break;
             }
         }
@@ -61,23 +67,85 @@ public class Principal {
         return option;
     }
 
+    private int getSaveOption() {
+        System.out.println(UserMessages.saveOption());
+        int option= Input.enterValidInput(reader);
+        System.out.println(UserMessages.returnOption(option));
+        reader.nextLine();
+        return option;
+    }
+
     private void searchBookByTitle(){
         System.out.println(UserMessages.searchBook());
         String bookTitle = reader.nextLine();
-        Book book = controller.getBook(bookTitle);
-        System.out.println(book);
-        Author author = book.getAuthor();
-        author.addToBookList(book);
-        repository.save(author);
+        BookDTO bookDTO;
+        try {
+            bookDTO = controller.searchBook(bookTitle);
+        } catch (NameNotFoundException e) {
+            System.out.println(UserMessages.notFoundMessage(bookTitle));
+            return;
+        }
+        Book newBook = controller.getBook(bookDTO);
+        System.out.println(newBook);
+        boolean retry= true;
+        while (retry){
+            int option = getSaveOption();
+            switch (option){
+                case 1:
+                    List<Book> bookList = new ArrayList<>();
+                    Author author = bookDTO.auth().stream().map(Author::new).toList().get(0);
+                    Optional<Author> authorQuery= repository.findByName(author.getName());
+                    if (authorQuery.isEmpty()){
+                        bookList.add(newBook);
+                        author.setBookList(bookList);
+                        repository.save(author);
+                        System.out.println(UserMessages.savedMessage());
+                        return;
+                    }
+                    Optional<Book> bookQuery = repository.searchBook(newBook.getTitle(), author.getName());
+                    if (bookQuery.isEmpty()){
+                        author= authorQuery.get();
+                        List<Book> oldBooks = repository.searchBooksFromAuthors(author.getName());
+                        bookList.addAll(oldBooks);
+                        bookList.add(newBook);
+                        author.setBookList(bookList);
+                        repository.save(author);
+                        System.out.println(UserMessages.savedMessage());
+                    }else {
+                        System.out.println(UserMessages.alreadyExistsMessage());
+                    }
+                    return;
+                case 2:
+                    retry=false;
+                    break;
+                default:
+                    System.out.println(UserMessages.invalidOption());
+                    break;
+            }
+        }
     }
 
     private void listSearchedBooks () {
-        List<Book> books= repository.buscarTodosLosLibros();
-        System.out.println(books);
+        List<Book> books= repository.searchAllBooks();
+        books.forEach(l -> System.out.printf("""
+                ----- BOOK -----
+                Title: %s
+                Author: %s
+                Language: %s
+                Download count: %d
+                %n""", l.getTitle(),l.getAuthor().getName(),l.getLanguage(),l.getDownloadCount()));
     }
 
     private void listAuthors () {
-
+        List<Author> authors= repository.findAll();
+        authors.forEach(a -> System.out.printf("""
+                ----- Author -----
+                Name: %s
+                Birth year: %d
+                Death year: %d
+                Books: %s
+                """.formatted(a.getName(),a.getBirth_year(),a.getDeath_year(),a.getBookList().stream()
+                .map(Book::getTitle).collect(Collectors.toList()))));
     }
 
     private void listAuthorAlive() {
