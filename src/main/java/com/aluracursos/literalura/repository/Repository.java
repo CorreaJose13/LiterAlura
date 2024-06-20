8ea070a1c4934de9c78d5a7367af94e4ec2ca9a7
CorreaJose13 <correa.jose@correounivalle.edu.co>
2024-06-19 21:30:13 -0500

    Add and modifiy querys in jpa repo

--- src/main/java/com/aluracursos/literalura/repository/Repository.java
+++ src/main/java/com/aluracursos/literalura/repository/Repository.java
@@ -2,13 +2,24 @@ package com.aluracursos.literalura.repository;
 
 import com.aluracursos.literalura.model.Author;
 import com.aluracursos.literalura.model.Book;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface Repository extends JpaRepository<Author, Long> {
 
+    Optional<Author> findByName(String name);
+
+    @Query("SELECT l FROM Author a JOIN a.bookList l WHERE a.name = :name AND l.title = :title")
+    Optional<Book> searchBook(String title, String name);
+
+    @Query("SELECT l FROM Author a JOIN a.bookList l WHERE a.name = :name")
+    List<Book> searchBooksFromAuthors(String name);
+
     @Query("SELECT l FROM Author a JOIN a.bookList l")
-    List<Book> buscarTodosLosLibros();
+    List<Book> searchAllBooks();
 }
